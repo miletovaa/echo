bc3c3be3fb39b8fca921f8ac5396557a3507fa52
anna miletova <annamiletova@gmail.com>
2025-07-26 22:43:35 +0200

    Echo task

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const debounceTimeout: number = 200
+  const [inputValue, setInputValue] = useState<string>("")
+  const [outputValue, setOutputValue] = useState<string>("")
+
+  const fetchOutput = async (value: string) => {
+    try {
+      const response = await fetch('https://burelli.xyz/api/exercises/echo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          text: value
+        }),
+      })
+
+      const data = await response.json()
+      return data.text || "ERROR IN API RESPONSE"
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
+
+  const handleInputChange = async (value: string) => {
+    localStorage.setItem('inputValue', value)
+
+    const output = await fetchOutput(value)
+    setOutputValue(output)
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (inputValue) {
+        handleInputChange(inputValue)
+      }
+    }, debounceTimeout)
+
+    return () => clearTimeout(timeout)
+  }, [inputValue])
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem('inputValue')
+    if (storedValue) {
+      setInputValue(storedValue)
+    }
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Echo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h3>Input</h3>
+        <input type="text" id="input" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <h3>Output</h3>
+        <input type="text" id="output" value={outputValue} readOnly />
+      </div>
     </>
   )
 }
